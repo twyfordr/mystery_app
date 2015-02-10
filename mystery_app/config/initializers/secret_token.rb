df3a85d932f7cdbb31afceb4ea2b2fcb40d3efc9
Rachel Twyford <rachel@twyford.us>
2015-02-09 19:53:45 -0600

    rails app scaffolding

---
+++ mystery_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysteryApp::Application.config.secret_token = '6be9b50ce3c2efd9c6e04b93658dcec07617e90e47bc9ac9d785a4840a9b90ff8c0a8df1b9f29e520810572dc035e97164ac8923220fedc42f157045bb386fd1'
