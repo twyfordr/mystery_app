3988f50f00ffed0b1b2738eb46ed506ac0296904
Rachel Twyford <rachel@twyford.us>
2015-02-11 12:44:59 -0600

    sign-in, sign-out links in a header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,3 @@
 class HomeController < ActionController::Base
+  layout 'application'
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def sign_in_link
+    if user_signed_in? 
+      link_to "Sign out", destroy_user_session_path,  :method => :delete 
+    else
+      link_to "Sign in",  new_user_session_path   
+    end
+  end
 end
