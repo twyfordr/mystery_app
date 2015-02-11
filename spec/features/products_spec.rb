30ce0dec0716e30691d7124b08c534f00b908238
Rachel Twyford <rachel@twyford.us>
2015-02-10 22:21:25 -0600

    basic products list

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 feature 'products' do
   scenario 'unauthed user lists products' do
-    Product.create :name => 'Monkey', :price => 1.00
+    Product.create! :name => 'Monkey', :price => 1.00
     visit products_path
 
     within '.list' do
-      page.should include('Monkey')
+      expect(page).to have_content('Monkey')
     end
   end
 end
