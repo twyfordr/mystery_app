ffc49f2e8b57da1532bdb5a5ce7a4c583311b558
Rachel Twyford <rachel@twyford.us>
2015-02-11 20:58:58 -0500

    bootstrap all the things

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'devise'
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 2.1.0.0'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,9 @@
 module ProductsHelper
   def sign_in_link
     if user_signed_in? 
-      link_to "Sign out", destroy_user_session_path,  :method => :delete 
+      link_to "Sign out", destroy_user_session_path,  :method => :delete, :class => 'pull-right' 
     else
-      link_to "Sign in",  new_user_session_path   
+      link_to "Sign in",  new_user_session_path , :class => 'pull-right'  
     end
   end
 end
